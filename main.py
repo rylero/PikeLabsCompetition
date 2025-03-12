2ca63558e1501980f4e1c8f1f0fe481e594f6309
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-03-11 19:00:55 -0700

    merged datasets

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ dataset_utils.download_kaggle_dataset('mayobanexsantana/political-bias')
 dataset_1 = dataset_utils.load_dataset('Political_Bias.csv')
 dataset_2 = dataset_utils.load_dataset('file_name.csv')
 
-print(dataset_1.head())
-print(dataset_2.head())
+dataset_1["political_bias"] = dataset_1["Bias"].map({"left": -1, "lean left": -0.5, "center left": 0, "center right": 0.5, "lean right": 1, "right": 1})
+dataset_2["political_bias"] = dataset_2["Political Lean"].map({"Liberal": -1, "Conservative": 1})
+
+merged_dataset = pd.merge(dataset_1, dataset_2, on="Text", how="inner")
+
+print(merged_dataset.head())
