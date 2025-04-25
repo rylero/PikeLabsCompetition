c655bf9099397d937f44f0d8582588a6b21ffc0a
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-04-25 09:50:30 -0700

    Grok with better link gathering

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Datasets:
 - https://www.kaggle.com/datasets/mayobanexsantana/political-bias
 
 To download datasets authenticate with kaggle
+
+No longer using custom ai model
+Switching to GROK completition with custom Brave Search API tooling
+
+Speed Comparison of Models (on this article https://www.npr.org/2025/04/22/nx-s1-5372588/trump-tariffs-imf-trade-world-economy):
+| Model    | Speed |
+| -------- | ------- |
+| GROK-3   | 1m 17.9s |
+| GROK-3-Fast | 25.57s |
+| GROK-3-Mini    | 45.13s |
+| GROK-3-Mini-Fast (chosen model)    | 28.46s |
--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ app = fastapi.FastAPI()
 
 from pydantic import BaseModel
 class Article(BaseModel):
-     body: str
+    body: str
 
 @app.get("/")
 def read_root():
@@ -29,7 +29,7 @@ def read_root():
 async def generate_report(url: Annotated[str, Form()], text: Annotated[str, Form()]):
     messages = [{
                 "role": "system",
-                "content": "You are a news fact checker. You get given a url and article and you return a json containing a factuality score out of 5, plus a description of why you chose that score. Factuality should be evaulated thrououghly. Include if other articles came to the same conclusion, even if there were from media sources that tipically reported from the opposite side. Also evaluate if the articles sources are correct, just because the article includes external sources doesnt mean that it is more factual.Then you get a bias score between -2, to +2 with -2 being very left leaning and +2 being very right leaning, along with another description of why. In your descriptions please use key sentences or phrases that leads you to your answer. answer in a json format with 4 fields: factuality, factuality-description, bias, bias-description. Also find news articles that oppose the current article using the \"search\" tool and gather a list of news article links and store them in the json called opposing_links. Then do the same with agreement_links with articles that agree with the current argument."
+                "content": "You are a news fact checker. You get given a url and article and you return a json containing a factuality score out of 5, plus a description of why you chose that score. Factuality should be evaulated thrououghly. Include if other articles came to the same conclusion, even if there were from media sources that tipically reported from the opposite side. Also evaluate if the articles sources are correct, just because the article includes external sources doesnt mean that it is more factual.Then you get a bias score between -2, to +2 with -2 being very left leaning and +2 being very right leaning, along with another description of why. In your descriptions please use key sentences or phrases that leads you to your answer. answer in a json format with 4 fields: factuality, factuality-description, bias, bias-description. Also find news articles that oppose the current article using the \"search\" tool and gather a list of news article links and store them in the json called opposing_links. You should start your search with the phase: \"News articles for ___\" or \"News articles against ___\". Then do the same with agreement_links with articles that agree with the current argument."
             },
             {
                 "role": "user",
@@ -37,7 +37,7 @@ async def generate_report(url: Annotated[str, Form()], text: Annotated[str, Form
             }]
     
     response = client.chat.completions.create(
-        model="grok-3",
+        model="grok-3-mini-fast",
         tools=tools_definition,
         tool_choice="auto",
         messages=messages
@@ -45,7 +45,7 @@ async def generate_report(url: Annotated[str, Form()], text: Annotated[str, Form
 
     messages.append(response.choices[0].message)
 
-    if response.choices[0].message.tool_calls:
+    while response.choices[0].message.tool_calls:
         for tool_call in response.choices[0].message.tool_calls:
 
             function_name = tool_call.function.name
@@ -68,7 +68,7 @@ async def generate_report(url: Annotated[str, Form()], text: Annotated[str, Form
             tool_choice="auto"
         )
 
-    return json.loads(response.choices[0].message.content)
+    return json.loads(response.choices[0].message.content);
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
